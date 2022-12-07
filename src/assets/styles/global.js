c648b5c8501abfd106d70c0a8cf381840ce67db5
gustavocsouza <gustavo.c.souza1991@gmail.com>
2022-12-06 21:50:31 -0300

    remove click blue color on mobile

--- src/assets/styles/global.js
+++ src/assets/styles/global.js
@@ -1,20 +1,28 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
- * {
-   margin: 0;
-   padding: 0;
-   box-sizing: border-box;
-   font-family: "Poppins", sans-serif;
- }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+  }
 
- body {
-   background: ${({ theme }) => theme.colors.background};
-   font-size: 16px;
-   color: ${({ theme }) => theme.colors.gray[900]};
- }
+  body {
+    background: ${({ theme }) => theme.colors.background};
+    font-size: 16px;
+    color: ${({ theme }) => theme.colors.gray[900]};
+  }
 
- button {
-   cursor: pointer;
- }
+  button {
+    cursor: pointer;
+  }
+
+  input,
+  textarea,
+  button,
+  select,
+  a {
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+  }
 `;
